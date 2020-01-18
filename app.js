d13c61a72d962e96a9afc029339e9850c18370ed
tinkugupta5 <tinku.gupta4@gmail.com>
2020-01-19 01:42:59 +0530

    adding winning column

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".btn-roll").addEventListener("click", function() {
+document.querySelector(".btn-roll").addEventListener("click", function () {
 	//Do something here
 	var dice = Math.floor(Math.random() * 6) + 1;
 
@@ -42,24 +42,63 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
 		document.querySelector("#current-" + activePlayer).textContent = roundScore;
 	} else {
 		// Next Player
-		activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-		roundScore = 0;
-
-		// if (activePlayer === 0) {
-		//     activePlayer = 1;
-
-		// } else
-
-		// {
-		//     activePlayer = 0;
-		// }
-
-		document.getElementById("current-0").textContent = "0";
-		document.getElementById("current-1").textContent = "0";
-
-		document.querySelector(".player-0-panel").classList.toggle("active");
-		document.querySelector(".player-1-panel").classList.toggle("active");
-
-		document.querySelector(".dice").style.display = "none";
+		nextPlayer();
 	}
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+
+	// ADD 	current score to global score
+	scores[activePlayer] += roundScore;
+
+
+
+
+
+
+	//update the UI 
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+
+	// CHECK WHEATHER PLAYER WON THE GAME OR NOT
+
+	//check if player won the game 
+
+	if (scores[activePlayer] >= 20) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+
+		nextPlayer();
+	}
+
+
+
+});
+
+function nextPlayer() {
+
+	// Next Player
+	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+	roundScore = 0;
+
+	// if (activePlayer === 0) {
+	//     activePlayer = 1;
+
+	// } else
+
+	// {
+	//     activePlayer = 0;
+	// }
+
+	document.getElementById("current-0").textContent = "0";
+	document.getElementById("current-1").textContent = "0";
+
+	document.querySelector(".player-0-panel").classList.toggle("active");
+	document.querySelector(".player-1-panel").classList.toggle("active");
+
+	document.querySelector(".dice").style.display = "none";
+
+}
