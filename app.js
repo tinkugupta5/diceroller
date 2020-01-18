0380b2d12a43e39524caa904ed45e854f252690a
tinkugupta5 <tinku.gupta4@gmail.com>
2020-01-18 23:00:30 +0530

    add toggle from active class to unactive class

--- app.js
+++ app.js
@@ -67,6 +67,9 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
 
     }
 
