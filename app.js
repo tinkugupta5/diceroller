bcb21a7f089a6749603d77b3129b24926a8519c9
tinkugupta5 <tinku.gupta4@gmail.com>
2020-01-18 22:42:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, dice;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// var x = document.querySelector('#score-1').textContent
+// console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    //Do something here 
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the result 
+
+    var diceDOM =
+        document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+
+    //3. Updte the round score If the rolled number was NOT a 1 
+
+    if (dice !== 1) {
+
+        roundScore = roundScore + dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else
+
+    {
+
+        // Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        // if (activePlayer === 0) {
+        //     activePlayer = 1;
+
+        // } else
+
+        // {
+        //     activePlayer = 0;
+        // }
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+
+    }
+
+
+
+});
